1fa418e6b9844b57b0f6b9ea02711f3274449e58
syu-shu <syuriku0126@gmail.com>
2024-07-23 09:58:21 +0900

    [remove] some temporally files

--- src/sigdim_profile.py
+++
@@ -1,8 +0,0 @@
-from sigdim import *
-import numpy as np
-
-d = 4
-state = np.concatenate([np.eye(d, dtype=np.int16), -np.eye(d, dtype=np.int16)])
-state = np.concatenate([state, np.ones((state.shape[0], 1), dtype=np.int16)], axis=1)
-
-print(sigdim(state))
