7535dcd9030a725840f364d9e7bbd824c8821bc5
syu-shu <syuriku0126@gmail.com>
2025-06-24 11:36:44 +0900

    [add] vertices in c

---
+++ src/lib_vertices.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// #define MAX_CHOICE 6
+
+int recfun();
+
+int vertices (int max, int pw, int ph, int P[ph][pw], int aw, int ah, int A[ah][aw]){
+    int *choice = malloc(sizeof(int) * max);
+    return recfun(0, 0, 0, choice, 0, max, pw, ph, P, aw, ah, A);
+}
+
+int recfun(int pi, int ai, int column, int* choice, int count, int max, int pw, int ph, int P[ph][pw], int aw, int ah, int A[ah][aw]){
+    if (pi == ph) {
+        memcpy(A[ai], choice, aw * sizeof(int));
+        return 1;
+    }
+    int diff = 0;
+    if (count < max){
+        for (int i = 0; i < pw; i++) {
+            if (P[pi][i] > 0 && count + (((column & (1 << i)) == 0)) <= max) {
+                choice[pi] = i;
+                diff += recfun(pi + 1, ai + diff, column | (1 << i), choice, count + (((column & (1 << i)) == 0)), max, pw, ph, P, aw, ah, A);
+            }
+        }
+    } else if (count == max) {
+        for (int i = 0; i < pw; i++) {
+            if ((column & (1 << i)) > 0 && P[pi][i] > 0) {
+                choice[pi] = i;
+                diff += recfun(pi + 1, ai + diff, column, choice, count, max, pw, ph, P, aw, ah, A);
+            }
+        }
+    }
+    return diff;
+}
